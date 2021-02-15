54fb92f4a14c06772eb2251643e73b1cc404e5a2
martinherm <mherman3103@gmail.com>
2021-02-15 17:17:09 +0100

    Remove todo

--- bird_view/utils/map_utils.py
+++ bird_view/utils/map_utils.py
@@ -890,7 +890,6 @@ class ModuleWorld(object):
 
     def _render_walkers(self, surface, list_w, world_to_pixel, from_snapshot=False):
         # print ("Walkers")
-        # TODO: What's the deal with pygame trying to render AIWalkerControllers?
         list_w = [walker for walker in list_w if walker[0].type_id != "controller.ai.walker"]
 
         for w in list_w:
