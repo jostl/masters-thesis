60d60a48d9d94b99f6a17d754c6923e8a05397ab
jostn <jostl@stud.ntnu.no>
2021-03-12 10:58:59 +0100

    Add accuracy within threshold calculation

--- perception/evaluation/model_evaluation.py
+++ perception/evaluation/model_evaluation.py
@@ -1,19 +1,56 @@
-from torch.utils.data import DataLoader
+import math
+from collections import defaultdict
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import DataLoader
+
 from perception.custom_datasets import ComparisonDataset
-from perception.utils.visualization import display_originals_with_decoded, display_batch_comparison
+
+
+def accuracy_with_threshold(targets, predictions, threshold=1.25):
+    """
+    Depth estimation evaluation method. Calculates a delta value for each pixel in an image, and
+    then checks which percentage of pixels are within a certain threshold.
+    Should work for batches and single images
+    """
+
+    targets_over_preds = targets / predictions
+    preds_over_targets = predictions / targets
+
+    deltas = np.maximum(targets_over_preds, preds_over_targets)
+
+    within_threshold_matrix = deltas < threshold
+
+    _, counts = np.unique(within_threshold_matrix, return_counts=True)
+
+    accuracy = counts[1] / (counts[0] + counts[1])  # this will work as long as there are both True and False values
+
+    return accuracy
+
+
+def compute_depth_loss(targets, predictions):
+    pass
 
 
 def compare_models(data_folder, segmentation_models, depth_models):
     targets = ComparisonDataset(data_folder, segmentation_models, depth_models, n_semantic_classes=6,
-                                max_n_instances=7)
+                                max_n_instances=15)
 
-    dataloader = DataLoader(targets, batch_size=5, shuffle=False, num_workers=0,
+    dataloader = DataLoader(targets, batch_size=10, shuffle=False, num_workers=0,
                             pin_memory=True)
 
-    for rgb_target, segmentation_target, depth_target, segmentation_preds, depth_preds in dataloader:
-        pass
+    accuracy_with_threshold_accumulated = defaultdict(int)
+    for rgb_targets, segmentation_targets, depth_targets, segmentation_preds, depth_preds in dataloader:
+        # TODO plot for sammenligning
+        for model in segmentation_preds:
+            pass
+
+        for model in depth_preds:
+             accuracy_with_threshold_accumulated[model] += accuracy_with_threshold(depth_targets, depth_preds[model])
+
+
 
 
 if __name__ == "__main__":
