0d7be012fbd35d4dc36fd0ad1ee6c08f79ba1f7c
jostn <jostl@stud.ntnu.no>
2021-03-24 14:58:07 +0100

    Add DeepLabv3 model training pipeline

---
+++ perception/deeplabv3/models.py
@@ -0,0 +1,18 @@
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+from torchvision import models
+
+
+def createDeepLabv3(outputchannels=1):
+    """DeepLabv3 class with custom head
+    Args:
+        outputchannels (int, optional): The number of output channels
+        in your dataset masks. Defaults to 1.
+    Returns:
+        model: Returns the DeepLabv3 model with the ResNet101 backbone.
+    """
+
+    model = models.segmentation.deeplabv3_resnet50(pretrained=True,
+                                                    progress=True)
+    model.classifier = DeepLabHead(2048, outputchannels)
+
+    return model
