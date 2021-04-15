a3e2ba19fbea96b684420a08e3b282ec5e7a5f64
jostn <jostl@stud.ntnu.no>
2021-04-15 13:09:16 +0200

    Fix error in models.py and add num_workers to train_semseg.py

--- perception/deeplabv3/models.py
+++ perception/deeplabv3/models.py
@@ -38,7 +38,7 @@ def createDeepLabv3(outputchannels=1, backbone="resnet50", pretrained=True):
 def createFCN(outputchannels=1, backbone="resnet50", pretrained=True):
     if backbone == "resnet50":
         print("FCN: Using resnet50 as backbone")
-        model = models.segmentation.deeplabv3_resnet50(pretrained=pretrained, progress=True,
+        model = models.segmentation.fcn_resnet50(pretrained=pretrained, progress=True,
                                                        num_classes=21, aux_loss=False)
     else:
         print("FCN: Using resnet101 as backbone")
@@ -61,18 +61,29 @@ def createMidas(use_large_model=True):
     else:
         midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
 
-    #return midas
-
+    return midas
+    """
     midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 
     if use_large_model:
         transform = midas_transforms.default_transform
     else:
         transform = midas_transforms.small_transform
-    return midas
-    img = cv2.imread(filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    import cv2
+    import urllib.request
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+    urllib.request.urlretrieve(url, filename)
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    midas.to(device)
+    midas.eval()
+
+    img = cv2.imread("data/perception/test1/rgb/clear_noon_1823_463.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     input_batch = transform(img).to(device)
 
     with torch.no_grad():
@@ -89,9 +100,10 @@ def createMidas(use_large_model=True):
 
     plt.imshow(output)
     plt.show()
+    """
 
 
 if __name__ == "__main__":
     #createDeepLabv3(outputchannels=9, backbone="resnet50", pretrained=True)
-    #createMidas()
-    createFCN(outputchannels=9, backbone="resnet101", pretrained=True)
+    createMidas()
+    #createFCN(outputchannels=9, backbone="resnet101", pretrained=True)
